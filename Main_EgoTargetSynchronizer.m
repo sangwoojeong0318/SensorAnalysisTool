10d95ac5e588126dc74e772e3bbb44fcf5cd0d32
Sangwoo <Sangwoojeong0318@gmail.com>
2018-02-10 11:13:57 +0900

    Erase unecessacy files

--- Main_EgoTargetSynchronizer.m
+++ Main_EgoTargetSynchronizer.m
@@ -105,11 +105,11 @@ if StereoAnalysis == true || RadarAnalysis == true
     
     if StereoAnalysis == true
         fnPlotObject(Object.Stereo.Associated, 'go');
-%         fnDrawBoundary(Object.Stereo);
+        fnDrawBoundary(Object.Stereo);
     end
     if RadarAnalysis == true
         fnPlotObject(Object.Radar, 'r^');
-%         fnDrawBoundary(Object.Radar);
+        fnDrawBoundary(Object.Radar);
     end
         
     if StereoAnalysis == true && RadarAnalysis == true
@@ -172,140 +172,3 @@ if StereoAnalysis == true || RadarAnalysis == true
     end
     hold off;
 end
-
-% % Draw radar
-% if RadarAnalysis == true
-%     figure(4);
-%     plot(EgoVehicle.enu(1, 1), EgoVehicle.enu(1, 2), 'r.');hold on;
-%     axis equal;
-%     grid on;
-%     xlabel('E(m)'); ylabel('N(m)');
-% 
-%     if ViewRadarVideo == true
-%         fnPlayVideo(EgoVehicle, TargetVehicle, Object.Radar);
-%     end
-% 
-%     cla;
-%     plot(EgoVehicle.enu(:,1), EgoVehicle.enu(:,2), 'r.'); hold on;
-%     plot(TargetVehicle.enu(:,1), TargetVehicle.enu(:,2), '.-');
-%     for idx = 1:1:size(Object.Radar.X_m, 1)
-%         figure(4);
-%         ObjectEnu = fnCoordCvt_local2enu(EgoVehicle.enu, EgoVehicle.sig_State_Hdg, Object.Radar.X_m(idx,:), Object.Radar.Y_m(idx,:), Object.Radar.Valid(idx,:));
-%         plot(ObjectEnu(:,1), ObjectEnu(:,2), 'g^');
-%     end
-%     legend('Ego vehicle', 'Target vehicle', 'Radar Obj');
-% end
-
-% 
-% TargetVel = SynchedInfo.A1Vel_kph';
-% % TargetAcc = CANoe_GPSDataFrames.Acc(Idx.TargetCANoe);
-% TargetHeading = SynchedInfo.A1Pos.Head';
-% TargetGPSValid = SynchedInfo.GPS.A1_GPSValid';
-% 
-% Idx.FlexRay = EgoPosLat';
-% EastErr = nan(length(Idx.FlexRay),1);
-% NorthErr = nan(length(Idx.FlexRay),1);
-% Pos_Long = nan(length(Idx.FlexRay),1);
-% Pos_Lat = nan(length(Idx.FlexRay),1);
-% Vx_mps = nan(length(Idx.FlexRay),1);
-% Vy_mps = nan(length(Idx.FlexRay),1);
-% HeadingDiff = nan(length(Idx.FlexRay),1);
-% TargetPosX = nan(length(Idx.FlexRay),1);
-% TargetPosY = nan(length(Idx.FlexRay),1);
-% TargetVelX = nan(length(Idx.FlexRay),1);
-% TargetVelY = nan(length(Idx.FlexRay),1);
-% TargetAccX = nan(length(Idx.FlexRay),1);
-% TargetAccY = nan(length(Idx.FlexRay),1);
-% 
-% % Reference Time is FlexRay time
-% for Erridx = 1 : 1: length(Idx.FlexRay)
-%     if TargetGPSValid(Erridx) == 4
-%         % Reference targe tvehicle position estimation
-%         RotMat = [cosd(-EgoVehicle.sig_State_Hdg(Erridx)) -sind(-EgoVehicle.sig_State_Hdg(Erridx));
-%             sind(-EgoVehicle.sig_State_Hdg(Erridx)) cosd(-EgoVehicle.sig_State_Hdg(Erridx))];
-% 
-%         tmp_CurrentPos = [TargetVehicle.enu(Erridx, 1) TargetVehicle.enu(Erridx, 2)]';
-%         tmp_CurrentPos_VehCoord = RotMat*tmp_CurrentPos;
-% 
-%         % Distance difference between ego and target vehicle
-%         EastErr(Erridx, 1) = (TargetVehicle.enu(Erridx, 1)) - EgoVehicle.enu(1);
-%         NorthErr(Erridx, 1) = (TargetVehicle.enu(Erridx, 2)) - EgoVehicle.enu(2) ;
-% 
-%         % Heading difference between ego and target vehicle
-%         HeadingDiff(Erridx, 1) = TargetHeading(Erridx, 1) - EgoVehicle.sig_State_Hdg(1); % [deg]
-% 
-%         % Target vehicle position respect to ego vehicle coordinate
-%         TargetPosX(Erridx, 1) = tmp_CurrentPos_VehCoord(1);
-%         TargetPosY(Erridx, 1) = tmp_CurrentPos_VehCoord(2);
-% 
-%         % Calculating the Vx and Vy
-%         TargetVelX(Erridx, 1) = (TargetVel(Erridx, 1) - EgoVel(1)) * cosd(HeadingDiff(Erridx, 1));
-%         TargetVelY(Erridx, 1) = (TargetVel(Erridx, 1) - EgoVel(1)) * sind(HeadingDiff(Erridx, 1));
-% % 
-% %         % Calculating the ax and ay
-% %         TargetAccX(Erridx, 1) = (TargetAcc(Erridx, 1) - EgoAcc(1)) * cosd(HeadingDiff(Erridx, 1));
-% %         TargetAccY(Erridx, 1) = (TargetAcc(Erridx, 1) - EgoAcc(1)) * sind(HeadingDiff(Erridx, 1));
-%     end
-% end
-% 
-% % Save reference data
-% Target_RefData.Time = TargetTime;
-% Target_RefData.EastErr = EastErr;
-% Target_RefData.NorthErr = NorthErr;
-% Target_RefData.PosX = TargetPosX;
-% Target_RefData.PosY = TargetPosY;
-% Target_RefData.Vel = TargetVel;
-% Target_RefData.VelX = TargetVelX;
-% Target_RefData.VelY = TargetVelY;
-% Target_RefData.AccX = TargetAccX;
-% Target_RefData.AccY = TargetAccY;
-% Target_RefData.Heading = HeadingDiff;
-% Target_RefData.ObjectFlag.Delphi = false(length(Target_RefData.PosX),1);
-% Target_RefData.ObjectFlag.Stereo = SynchedInfo.Validity;
-% Target_RefData.ObjectFlag.FusionBox = false(length(Target_RefData.PosX),1);
-% 
-% % Save stereo
-% tmp_StereoErrList.bTrackValid = SynchedInfo.AssoObj.Valid';
-% tmp_StereoErrList.Time = SynchedInfo.AssoObj.Time';
-% tmp_StereoErrList.NumofObj = SynchedInfo.AssoObj.Valid';
-% tmp_StereoErrList.nID = SynchedInfo.AssoObj.ID';
-% tmp_StereoErrList.Valid = SynchedInfo.AssoObj.Valid';
-% tmp_StereoErrList.PosX = SynchedInfo.AssoObj.X' - Target_RefData.PosX;
-% tmp_StereoErrList.PosY = SynchedInfo.AssoObj.Y' - Target_RefData.PosY;
-% tmp_StereoErrList.V = SynchedInfo.AssoObj.Vel_kph' - TargetVel;
-% tmp_StereoErrList.Vx = SynchedInfo.AssoObj.VelX_kph' - Target_RefData.VelX;
-% tmp_StereoErrList.Vy = SynchedInfo.AssoObj.VelY_kph' - Target_RefData.VelY;
-% tmp_StereoErrList.Width = SynchedInfo.AssoObj.Valid';
-% tmp_StereoErrList.Length = SynchedInfo.AssoObj.Valid';
-% 
-% for idx = 1:1:size(SynchedInfo.AssoObj.heading,2)
-%     HeadingErr = SynchedInfo.AssoObj.heading(idx);
-%     if HeadingErr >= 180
-%        HeadingErr =  HeadingErr - 360;
-%     end        
-%     tmp_StereoErrList.Heading(idx, 1) = HeadingErr';
-% end
-% 
-% tmp_StereoErrList.ref.Time = SynchedInfo.AssoObj.Time';
-% tmp_StereoErrList.ref.nID = SynchedInfo.AssoObj.ID';
-% tmp_StereoErrList.ref.Valid = SynchedInfo.AssoObj.Valid';
-% tmp_StereoErrList.ref.PosX = SynchedInfo.AssoObj.X';
-% tmp_StereoErrList.ref.PosY = SynchedInfo.AssoObj.Y';
-% tmp_StereoErrList.ref.V = SynchedInfo.AssoObj.Vel_kph';
-% tmp_StereoErrList.ref.Vx = SynchedInfo.AssoObj.VelX_kph';
-% tmp_StereoErrList.ref.Vy = SynchedInfo.AssoObj.VelY_kph';
-% tmp_StereoErrList.ref.Heading = SynchedInfo.AssoObj.heading';
-% tmp_StereoErrList.ref.Width = SynchedInfo.AssoObj.Valid';
-% tmp_StereoErrList.ref.Length = SynchedInfo.AssoObj.Valid';
-% 
-% 
-% aSensorErrList.Stereo = tmp_StereoErrList;
-% aSensorErrList.Reference = Target_RefData;
-% 
-% %% Save data
-% % Synched signals
-% tmp_Filename = strsplit(File_path,'\');
-% Filename = 'SynchedSignals.mat';
-% destination = File_path;
-% saveFile = fullfile(destination, Filename);
-% save(saveFile, 'aSensorErrList');
--- SensorType.m
+++
@@ -1,7 +0,0 @@
-classdef SensorType < uint8
-   enumeration
-      Delphi (0)
-      Mobileye (1)
-      FusionBox (2)
-   end
-end
