c50ff98e9a1ff44ecd4621363b245958429c7dd4
Sangwoo <sangwoojeong0318@gmail.com>
2018-02-19 09:11:06 +0900

    Temporal commit

--- Plot/Plot_object.m
+++ Plot/Plot_object.m
@@ -1,5 +1,5 @@
 clear all; addpath('functions');
-CAN_sig_path = 'F:\00_DB\DANGUN\SensorEvaluation\[180208]_[KATRI]\KATRI_Zoe1_008.mat';
+CAN_sig_path = 'D:\User\Sangwoo\git\SensorAnalysisTool\DB\180219\test\KATRI_Zoe1_008.mat';
 CAN_raw = load(CAN_sig_path);
 object_list_radar = fnGetRadarObj(CAN_raw);
 object_list_stereo = fnGetStereoObj(CAN_raw);
@@ -14,6 +14,7 @@ for i=1:size(k,2)
     obj = object_list_radar(k{i});
     figure(1); plot(obj.pos_y_m(:,2), obj.pos_x_m(:,2), '.'); 
     hold on; grid on; axis equal; xlim([-20, 20]); ylim([-5, 100]);
+    pause();
 end
 legend(leg_list);
 % 
@@ -34,5 +35,6 @@ for i=1:size(k,2)
     obj = object_list_stereo(k{i});
     figure(2); plot(obj.pos_y_m(:,2), obj.pos_x_m(:,2), '.'); 
     hold on; grid on; axis equal; xlim([-20, 20]); ylim([-5, 100]);
+    pause();
 end
 legend(leg_list);
